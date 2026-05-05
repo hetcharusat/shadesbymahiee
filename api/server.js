95b37fd387bee477d84d4dfdc96924d3617cdfba
hetcharusat <24dce091@charusat.edu.in>
2026-05-05 22:19:21 +0530

    fix: restore nodejs api handler and vercel.json with error wrapper

---
+++ api/server.js
@@ -0,0 +1,11 @@
+﻿import server from '../dist/server/server.js';
+
+export default async function(request) {
+  try {
+    if (request.url) {
+      return await server.fetch(request);
+    }
+  } catch (err) {
+    return new Response(err.stack || err.toString(), { status: 500 });
+  }
+}
